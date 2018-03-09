43dcbc552bc7ec5a1d109a06b18d40d48e8f0454
deccoder <declanbrophy@gmail.com>
2018-03-09 17:33:16 +0000

    Method for saving to database

--- app/src/main/java/declanbrophy/barrowrovers/CreateTeam.java
+++ app/src/main/java/declanbrophy/barrowrovers/CreateTeam.java
@@ -9,8 +9,11 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,7 +24,7 @@ public class CreateTeam extends AppCompatActivity {
     EditText teamName, systemAdmin, address, email;
     DatabaseReference databaseReference;
     private static String teamId;
-    ArrayList<teams> teams;
+    ArrayList<Team> teamOne;
 
 
     @Override
@@ -29,7 +32,7 @@ public class CreateTeam extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_team);
 
-        teams = new ArrayList<teams>();
+        teamOne = new ArrayList<Team>();
         databaseReference = FirebaseDatabase.getInstance().getReference("teams");
 
         save = (Button) findViewById(R.id.save);
@@ -47,27 +50,59 @@ public class CreateTeam extends AppCompatActivity {
                 if (TextUtils.isEmpty(teamId)) {
                     //save
                     String id = databaseReference.push().getKey();
-                    teams teams = new teams(teamId, name);
+                    Team teams = new Team(teamId, name);
                     databaseReference.child(teamId).setValue(teams);
                 }else if (TextUtils.isEmpty(teamId)) {
                     String id = databaseReference.push().getKey();
                     String sAdmin = systemAdmin.getText().toString();
-                    teams teams = new teams(id,sAdmin);
+                    Team teams = new Team(id,sAdmin);
                     databaseReference.child(id).setValue(teams);
                 }else if (TextUtils.isEmpty(teamId)) {
                     String id = databaseReference.push().getKey();
                     String teamAddress = address.getText().toString();
-                    teams teams = new teams(id, teamAddress);
+                    Team teams = new Team(id, teamAddress);
                     databaseReference.child(id).setValue(teams);
                 }else {
                     String id = databaseReference.push().getKey();
                     String teamEmail = email.getText().toString();
-                    teams teams = new teams(id,teamEmail);
+                    Team teams = new Team(id,teamEmail);
                     databaseReference.child(id).setValue(teams);
 
                     Toast.makeText(CreateTeam.this, "Team created successfully", Toast.LENGTH_SHORT).show();
                 }
+
+                teamName.setText(null);
+                systemAdmin.setText(null);
+                address.setText(null);
+                email.setText(null);
+            }
+
+
+        });
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                teamOne.clear();
+
+                for (DataSnapshot postSnapshot: dataSnapshot.getChildren()) {
+                    Team teams = postSnapshot.getValue(Team.class);
+                    teamOne.add(teams);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
             }
         });
     }
 }
+
