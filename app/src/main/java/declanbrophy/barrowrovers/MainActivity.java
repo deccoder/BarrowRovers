271562d3dce0114dc3c6774dacbc26a10f1411f8
deccoder <declanbrophy@gmail.com>
2018-03-13 16:28:52 +0000

    Validation code for log in and registration.

--- app/src/main/java/declanbrophy/barrowrovers/MainActivity.java
+++ app/src/main/java/declanbrophy/barrowrovers/MainActivity.java
@@ -1,38 +1,74 @@
 package declanbrophy.barrowrovers;
 
 import android.content.Intent;
+import android.nfc.Tag;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
     Button register,signIn;
     EditText emailAddress, password;
-
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
+        mAuth = FirebaseAuth.getInstance();
+
+
+
         register = (Button) findViewById(R.id.register);
         signIn = (Button) findViewById(R.id.signIn);
         emailAddress = (EditText) findViewById(R.id.emailAddress);
         password = (EditText) findViewById(R.id.password);
 
-        register.setOnClickListener(new View.OnClickListener() {
+        signIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String email = emailAddress.getText().toString();
+                String pWord = password.getText().toString();
+
+                if (TextUtils.isEmpty(email)){
+                    Toast.makeText(getApplicationContext(),"Enter email address", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(pWord)){
+                    Toast.makeText(getApplicationContext(),"Enter password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
                 Intent intent = new Intent(MainActivity.this, MainMenu.class);
                 startActivity(intent);
             }
         });
-        signIn.setOnClickListener(new View.OnClickListener() {
+        register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String email = emailAddress.getText().toString().trim();
+                String pWord = password.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)){
+                    Toast.makeText(getApplicationContext(),"Enter email address", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(pWord)){
+                    Toast.makeText(getApplicationContext(),"Enter password",Toast.LENGTH_SHORT).show();
+                    return;
+                }
                 Intent intent = new Intent(MainActivity.this, MainMenu.class);
                 startActivity(intent);
             }
