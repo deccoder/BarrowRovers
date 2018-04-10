d6969d77fc3a1983d641d2523edbe76963d354b5
deccoder <declanbrophy@gmail.com>
2018-04-10 21:53:00 +0100

    Reading team details from the database

---
+++ app/src/main/java/declanbrophy/barrowrovers/TeamList.java
@@ -0,0 +1,49 @@
+package declanbrophy.barrowrovers;
+
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by User on 10/04/2018.
+ */
+
+public class TeamList extends ArrayAdapter<Team> {
+
+    private Activity context;
+    private List<Team> teamList;
+
+    public TeamList(Activity context, List<Team> teamList){
+        super(context, R.layout.activity_view_team);
+        this.context = context;
+        this.teamList = teamList;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater layoutInflater = context.getLayoutInflater();
+
+        View listViewItem = layoutInflater.inflate(R.layout.activity_view_team, null, true);
+
+        TextView teamName = (TextView) listViewItem.findViewById(R.id.teamName);
+        TextView systemAdmin = (TextView) listViewItem.findViewById(R.id.systemAdmin);
+        TextView address = (TextView) listViewItem.findViewById(R.id.address);
+        TextView email = (TextView) listViewItem.findViewById(R.id.email);
+
+        Team team = teamList.get(position);
+
+        teamName.setText(team.getTeamName());
+        systemAdmin.setText(team.getSystemAdmin());
+        address.setText(team.getAddress());
+        email.setText(team.getEmail());
+
+        return  listViewItem;
+    }
+}
--- app/src/main/java/declanbrophy/barrowrovers/ViewTeam.java
+++ app/src/main/java/declanbrophy/barrowrovers/ViewTeam.java
@@ -2,16 +2,62 @@ package declanbrophy.barrowrovers;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ViewTeam extends AppCompatActivity {
 
     Team team;
+    ListView teamBox;
+    DatabaseReference teamDetails;
+    //Creates a list of teams
+    List<Team> teamList;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        teamDetails.addValueEventListener(new ValueEventListener() {
+            @Override
+            //This method will be executed anytime something is changed in the database
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                //Clears the teamlist each time it is called from the database
+                teamList.clear();
+
+                for (DataSnapshot teamSnapshot : dataSnapshot.getChildren()){
+                    //Using team object to get values
+                    Team team = teamSnapshot.getValue(Team.class);
+                    //Adds values to the team list
+                    teamList.add(team);
+                }
+
+                TeamList adapter = new TeamList(ViewTeam.this, teamList);
+                teamBox.setAdapter(adapter);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_team);
+
+        teamBox = (ListView) findViewById(R.id.textBox);
+        teamList = new ArrayList<>();
 
         team = (Team)getIntent().getSerializableExtra("team");
 
